e07f53f954104d2bb3225684096057d88da23ac4
crystalgrape <jieni@kiloway.com>
2020-08-25 17:04:53 +0800

    添加ide

---
+++ asn.Ide/Program.cs
@@ -0,0 +1,30 @@
+﻿using asn.Runtime.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asn.Ide
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //加载伪指令插件
+            DummyInsCompiler dummyInsCompiler = new DummyInsCompiler();
+            dummyInsCompiler.Load("dummys/");
+
+            //加载指令插件
+            Plugins optLoader = new Plugins();
+            optLoader.Load("plugins/");
+            Application.Run(new AsnForm());
+        }
+    }
+}
